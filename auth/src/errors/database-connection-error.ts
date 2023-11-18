ee41c3e183b845bd0e487be34e6dc759a49a52fe
진찬호 <chanhojin94@kakao.com>
2023-11-18 22:00:52 +0900

    Proper Error Handling

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,18 +1,16 @@
 import { CustomError } from "./custom-errors";
 
-export class DatabaseConnectionError extends Error implements CustomError {
+export class DatabaseConnectionError extends CustomError {
   statusCode = 500;
   reason = "Error connecting to database";
+
   constructor() {
     super("Error connecting to db");
 
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
   }
-  serializeErrors(): { message: string; field?: string | undefined }[] {
-    throw new Error("Method not implemented.");
-  }
 
-  serializeError() {
+  serializeErrors() {
     return [{ message: this.reason }];
   }
 }
